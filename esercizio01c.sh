30d40b6ca5919cad24fa7447a1940c2118447e5b
Danilo Pesce <dprlog890@gmail.com>
2026-03-31 07:09:55 +0000

    Esercizio 1

---
+++ esercizio01c.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ============================================================
+# VERIFICA BASH - Traccia C - Esercizio 1
+# Navigazione e directory
+# ============================================================
+# OBIETTIVO:
+#   1. Entra nella cartella ambiente/immagini/vacanze
+#      usando un path assoluto
+#   2. Verifica il percorso con pwd
+#   3. Spostati in ambiente/immagini/lavoro usando un path relativo
+#      (con ..)
+#   4. Verifica la nuova posizione
+#
+# Scrivi i comandi qui sotto:
+# ------------------------------------------------------------
+
+
+cd ambiente/immagini/vacanze
+pwd
+cd -
+cd ambiente/immagini/lavoro
+pwd
